6edfe46bf91a39be0c24fba6fcfd85498e8232c6
wajih tarsim <wajih.tarsim@esprit.tn>
2018-12-05 16:42:57 +0100

    OAuth client

---
+++ src/main/java/com/example/config/WebConfig.java
@@ -0,0 +1,39 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        super.addViewControllers(registry);
+        registry.addViewController("/")
+                .setViewName("forward:/index");
+        registry.addViewController("/index");
+        registry.addViewController("/secure");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources");
+    }
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer(){
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+    @Bean
+    public static RequestContextListener requestContextListener(){
+        return new RequestContextListener();
+    }
+}
